8d879af0c366e2b7d5876a746dc1c2ee3243c680
Jussi Salonen <jasalonen@users.noreply.github.com>
2016-11-18 07:40:27 +0200

    Update robot-execute.js

--- NodeRed/robot-execute.js
+++ NodeRed/robot-execute.js
@@ -1,35 +1,12 @@
-/**
- * Copyright 2014 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
 
-// If you use this as a template, update the copyright with your own name.
-
-// Sample Node-RED node file
 
 
 module.exports = function(RED) {
     "use strict";
-    // require any external libraries we may need....
-    //var foo = require("foo-library");
 
-    // The node definition
     function RobotExecuteNode(n) {
-        // Create a RED node
         RED.nodes.createNode(this,n);
 
-        // copy "this" object in case we need it in context of callbacks of other functions.
         var node = this;
 
         // respond to inputs....
@@ -57,14 +34,11 @@ module.exports = function(RED) {
         });
 
         this.on("close", function() {
-            // Called when the node is shutdown - eg on redeploy.
-            // Allows ports to be closed, connections dropped etc.
-            // eg: node.client.disconnect();
+
         });
     }
 
-    // Register the node by name. This must be called before overriding any of the
-    // Node functions.
+
     RED.nodes.registerType("robot-execute",RobotExecuteNode);
 
 }
