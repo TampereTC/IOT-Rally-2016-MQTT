90f79a537e3b8a63d65a14408b28971b25947809
Jussi Salonen <jasalonen@users.noreply.github.com>
2016-11-18 07:38:27 +0200

    Update robot-command.js

--- NodeRed/robot-command.js
+++ NodeRed/robot-command.js
@@ -1,39 +1,19 @@
-/**
- * Copyright 2014 IBM Corp.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
-
-// If you use this as a template, update the copyright with your own name.
-
-// Sample Node-RED node file
+//
+//  
+//
 
 
 module.exports = function(RED) {
     "use strict";
-    // require any external libraries we may need....
-    //var foo = require("foo-library");
-
+    
     // The node definition
     function RobotCommandNode(n) {
-        // Create a RED node
         RED.nodes.createNode(this,n);
 
         // Store node configuration properties
         this.time = n.time;
         this.command = n.command;
 
-        // copy "this" object in case we need it in context of callbacks of other functions.
         var node = this;
 
         node.log("Time: " + this.time);
